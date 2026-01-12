14f73b441274c8c9d037eb264d54d996ee2c8222
AN JAE MIN <jaeminan944@icloud.com>
2026-01-12 14:57:46 +0900

    Learn JS Data Type

---
+++ section01/chapter05.js
@@ -0,0 +1,27 @@
+// 1. Number Type
+let num1 = 27;
+let num2 = 1.5;
+let num3 = -20;
+
+let inf = Infinity;
+let mInf = -Infinity;
+
+let nan = NaN;
+
+// 2. String Type
+let myName = "Jaemin An";
+let myLocation = "Seoul, Korea";
+let introduction = myName + myLocation;
+
+let introductionText = `${myName} lives in ${myLocation}.`;
+
+// 3. Boolean Type
+let isSwithchedOn = true;
+let isEmpty = false;
+
+// 4. Null Type (아무것도 없다)
+let empty = null;
+
+// 5. Undefined Type (값이 할당되지 않음)
+let none;
+console.log(none); // undefined
